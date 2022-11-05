e55e179de1263eca36855fc65a7f723181253e6c
Adrian_Bronowski <229849@edu.p.lodz.pl>
2022-11-05 18:11:39 +0100

    Wstep

---
+++ src/app/logowanie-i-rejestracja/logowanie-i-rejestracja.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logowanie-i-rejestracja',
+  templateUrl: './logowanie-i-rejestracja.component.html',
+  styleUrls: ['./logowanie-i-rejestracja.component.css']
+})
+export class LogowanieIRejestracjaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/wyszukiwanie/wyszukiwanie.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WyszukiwanieComponent } from './wyszukiwanie.component';
+
+describe('WyszukiwanieComponent', () => {
+  let component: WyszukiwanieComponent;
+  let fixture: ComponentFixture<WyszukiwanieComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WyszukiwanieComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(WyszukiwanieComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
