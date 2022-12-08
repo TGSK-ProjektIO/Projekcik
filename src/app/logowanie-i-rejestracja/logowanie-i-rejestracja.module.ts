265695ef6dded832c3d30e31dfc1dc2e1560b193
Piotr Gągorowski <236533@edu.p.lodz.pl>
2022-12-08 22:24:28 +0100

    logowanie - widok

--- src/app/logowanie-i-rejestracja/logowanie-i-rejestracja.module.ts
+++ src/app/logowanie-i-rejestracja/logowanie-i-rejestracja.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LogowanieComponent } from './logowanie/logowanie.component';
+import { RejestracjaComponent } from './rejestracja/rejestracja.component';
+import { PotwierdzenieComponent } from './potwierdzenie/potwierdzenie.component';
+import { ResetowanieComponent } from './resetowanie/resetowanie.component';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        LogowanieComponent,
+        RejestracjaComponent,
+        PotwierdzenieComponent,
+        ResetowanieComponent
+    ],
+  exports: [
+    RejestracjaComponent,
+    PotwierdzenieComponent,
+    ResetowanieComponent,
+    LogowanieComponent
+  ],
+    imports: [
+        CommonModule
+    ]
 })
 export class LogowanieIRejestracjaModule { }
---
+++ src/app/logowanie-i-rejestracja/resetowanie/resetowanie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resetowanie',
+  templateUrl: './resetowanie.component.html',
+  styleUrls: ['./resetowanie.component.css']
+})
+export class ResetowanieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
