e8e457cd855e89d5af2b34bfa9e84f56b334e2a4
236662 Stróżtk Filip <236662@edu.p.lodz.pl>
2022-12-13 19:29:07 +0100

    Revert "Pierwsza integracja???"
    This reverts commit 0ea17d55d5aba3b345356ca785b39ac788560fc0.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,30 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from "@angular/forms";
-import { MatTableModule } from "@angular/material/table";
-import { MatSortModule } from "@angular/material/sort";
-import { MatInputModule } from "@angular/material/input";
-import { MatSelectModule } from "@angular/material/select";
-import { MatPaginatorIntl, MatPaginatorModule } from "@angular/material/paginator";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { RatingModule, RatingConfig } from 'ngx-bootstrap/rating';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-
 import { LogowanieIRejestracjaComponent } from './logowanie-i-rejestracja/logowanie-i-rejestracja.component';
 import { PanelUzytkownikaComponent } from './panel-uzytkownika/panel-uzytkownika.component';
 import { ProduktComponent } from './produkt/produkt.component';
 import { SugestieIZgloszeniaComponent } from './sugestie-i-zgloszenia/sugestie-i-zgloszenia.component';
-import { PolishPaginatorIntl, WyszukiwanieComponent} from './wyszukiwanie/wyszukiwanie.component';
+import {PolishPaginatorIntl, WyszukiwanieComponent} from './wyszukiwanie/wyszukiwanie.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AdministratorViewComponent } from './sugestie-i-zgloszenia/administrator-view/administrator-view.component';
 import { SugestieIZgloszeniaModule } from "./sugestie-i-zgloszenia/sugestie-i-zgloszenia.module";
-import { ProduktModule } from "./produkt/produkt.module";
+import {ProduktModule} from "./produkt/produkt.module";
 import { OpinieModule } from "./opinie/opinie.module";
-import {LogowanieIRejestracjaModule} from "./logowanie-i-rejestracja/logowanie-i-rejestracja.module";
-import { PanelUzytkownikaModule } from "./panel-uzytkownika/panel-uzytkownika.module";
 
+import {LogowanieIRejestracjaModule} from "./logowanie-i-rejestracja/logowanie-i-rejestracja.module";
+import { AppRoutingModule } from './app-routing.module';
+import {PanelUzytkownikaModule} from "./panel-uzytkownika/panel-uzytkownika.module";
+import {FormsModule} from "@angular/forms";
+import {MatTableModule} from "@angular/material/table";
+import {MatSortModule} from "@angular/material/sort";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {MatPaginatorIntl, MatPaginatorModule} from "@angular/material/paginator";
+import {MatCheckboxModule} from "@angular/material/checkbox";
 
 @NgModule({
   declarations: [
@@ -51,12 +51,12 @@ import { PanelUzytkownikaModule } from "./panel-uzytkownika/panel-uzytkownika.mo
     MatPaginatorModule,
     MatCheckboxModule,
     SugestieIZgloszeniaModule,
-    // ProduktModule,
+    ProduktModule,
     OpinieModule,
     RatingModule.forRoot(),
-    // PanelUzytkownikaModule,
+    PanelUzytkownikaModule,
     RouterOutlet,
-    LogowanieIRejestracjaModule,
+    // LogowanieIRejestracjaModule,
     AppRoutingModule
   ],
   providers: [RatingConfig, {provide: MatPaginatorIntl, useClass: PolishPaginatorIntl}],
