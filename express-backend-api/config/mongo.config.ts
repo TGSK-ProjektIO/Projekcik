430859fabebb95214a629c7232fe478b03b564cb
236662 <236662@edu.p.lodz.pl>
2022-12-13 16:51:28 +0100

    database change + small fix

--- express-backend-api/config/mongo.config.ts
+++ express-backend-api/config/mongo.config.ts
@@ -1,4 +1,4 @@
-export const DATABASE_URi = `mongodb+srv://student:8wb7hZ7OnPq5T9GE@cluster0.1apnqzx.mongodb.net/?retryWrites=true&w=majority`;
+export const DATABASE_URi = `mongodb+srv://dbUser12345:1tUA6SRk7CzvwO5h@bazkidlainzynieriioprog.owe1f.mongodb.net/?retryWrites=true&w=majority`;
 export const DB_NAME = 'TEST';
 export const USER_COLLECTION_NAME = 'USERS';
 export const SESSION_COLLECTION_NAME = 'SESSIONS';
