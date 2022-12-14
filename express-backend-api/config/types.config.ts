f4d7adc92fa7b4930351c4ea681cfb21393f2530
LiptonLeon <93220207+LiptonLeon@users.noreply.github.com>
2022-12-14 23:57:56 +0100

    feat: ProfileService

--- express-backend-api/config/types.config.ts
+++ express-backend-api/config/types.config.ts
@@ -3,15 +3,19 @@ import {SessionService} from "../services/session.service";
 const TYPES = {
   UserController: Symbol.for('UserController'),
   SessionController: Symbol.for('SessionController'),
+  ProfileController: Symbol.for('ProfileController'),
 
   UserRepository: Symbol.for('UserRepository'),
   SessionRepository: Symbol.for('SessionRepository'),
+  ProfileRepository: Symbol.for('ProfileRepository'),
 
   UserRouter: Symbol.for('UserRouter'),
   SessionRouter: Symbol.for('SessionRouter'),
+  ProfileRouter: Symbol.for('ProfileRouter'),
 
   UserService: Symbol.for('UserService'),
-  SessionService: Symbol.for('SessionService')
+  SessionService: Symbol.for('SessionService'),
+  ProfileService: Symbol.for('ProfileService')
 };
 
 export { TYPES };
