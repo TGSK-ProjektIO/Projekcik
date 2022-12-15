597ff620c9a4b652696c115bad944bb310254c89
EmiNiemand <Szymon_PL@live.com>
2022-12-15 19:01:19 +0100

    add frontend from previous fork

--- src/app/opinie/opinie.module.ts
+++ src/app/opinie/opinie.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OpinieComponent } from './opinie.component';
 import { ReviewComponent } from './review/review.component';
-import { OpinionRatingComponent } from './opinion-rating/opinion-rating.component';
 import { RatingComponent } from './rating/rating.component';
+import { OpinionRatingComponent } from './opinion-rating/opinion-rating.component';
+import { RatingModule } from "ngx-bootstrap/rating";
+import { FormsModule } from "@angular/forms";
+import { CompleteOpinionComponent } from "./complete-opinion/complete-opinion.component";
+import { OpinionHostDirective } from './opinion-host.directive';
+import { OpinionCreatorComponent } from "./opinion-creator/opinion-creator.component";
 
 
 
@@ -11,11 +16,22 @@ import { RatingComponent } from './rating/rating.component';
   declarations: [
     OpinieComponent,
     ReviewComponent,
+    RatingComponent,
     OpinionRatingComponent,
-    RatingComponent
+    CompleteOpinionComponent,
+    OpinionHostDirective,
+    OpinionCreatorComponent
+  ],
+  exports: [
+    OpinieComponent,
+    CompleteOpinionComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RatingModule,
+    FormsModule
   ]
 })
-export class OpinieModule { }
+export class OpinieModule {
+
+}
---
+++ src/app/opinie/opinion-rating/opinion-rating.component.ts
@@ -0,0 +1,62 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+enum OpinionRatingState { None, Liked, Disliked }
+
+@Component({
+  selector: 'app-opinion-rating',
+  templateUrl: './opinion-rating.component.html',
+  styleUrls: ['./opinion-rating.component.css']
+})
+export class OpinionRatingComponent implements OnInit {
+  @Input() likes : number = 0;
+  @Input() dislikes : number = 0;
+  @Input() ratingState : OpinionRatingState = OpinionRatingState.None;
+  @Input() isReadonly = true;
+
+  constructor() {}
+  ngOnInit(): void {}
+
+  ClickedLike() : void {
+    switch (this.ratingState) {
+      case OpinionRatingState.Disliked:
+        this.RemoveDislike();
+        this.AddLike();
+        this.ratingState = OpinionRatingState.Liked;
+        break;
+      case OpinionRatingState.None:
+        this.AddLike();
+        this.ratingState = OpinionRatingState.Liked;
+        break;
+      case OpinionRatingState.Liked:
+        this.RemoveLike();
+        this.ratingState = OpinionRatingState.None;
+        break;
+    }
+  }
+
+  ClickedDislike() : void {
+    switch (this.ratingState) {
+      case OpinionRatingState.Liked:
+        this.RemoveLike();
+        this.AddDislike();
+        this.ratingState = OpinionRatingState.Disliked;
+        break;
+      case OpinionRatingState.None:
+        this.AddDislike();
+        this.ratingState = OpinionRatingState.Disliked;
+        break;
+      case OpinionRatingState.Disliked:
+        this.RemoveDislike();
+        this.ratingState = OpinionRatingState.None;
+        break;
+    }
+  }
+
+  private AddLike() : void { this.likes += 1;  }
+  private AddDislike() : void { this.dislikes += 1; }
+  private RemoveLike() : void { this.likes -= 1; }
+  private RemoveDislike() : void { this.dislikes -= 1; }
+  GetRating() : string {
+    return `This opinion has ${this.likes} likes and ${this.dislikes} dislikes.`
+  }
+}
