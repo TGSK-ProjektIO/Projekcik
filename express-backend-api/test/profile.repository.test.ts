5743dcc397415f527e2931ad1b3831bbd778561a
LiptonLeon <krystian.baraniecki@gmail.com>
2022-12-16 21:17:01 +0100

    feat: ProfileRepository tests

--- express-backend-api/test/profile.repository.test.ts
+++ express-backend-api/test/profile.repository.test.ts
@@ -6,7 +6,6 @@ import {faker} from "@faker-js/faker";
 import {ObjectId} from "mongodb";
 import {create} from "domain";
 import {Profile} from "../model/profile";
-import test from "node:test";
 
 const profileRepository = container.get<ProfileRepository>(TYPES.ProfileRepository);
 
@@ -71,6 +70,13 @@ test('Read Profile by nickname negative test', async () => {
   await expect(profileRepository.readByNickname("nullptr")).rejects.toBeUndefined();
 });
 
+test('Read all Profile positive test', async () => {
+  let createdProfile = await profileRepository.create(validProfile);
+
+  let readProfile = await profileRepository.readAll();
+  expect(readProfile).toContainEqual(createdProfile);
+});
+
 test('Update profile positive test', async () => {
   let createdProfile = await profileRepository.create(validProfile);
   createdProfile.nickname = faker.system.filePath();
@@ -89,71 +95,9 @@ test('Delete profile positive test', async () => {
 
 test('Delete profile negative test', async () => {
   let createdProfile = await profileRepository.create(validProfile);
-  await expect(userRepository.delete(new ObjectId().toString())).rejects.toBeUndefined();
+  await expect(profileRepository.delete("nullptr")).rejects.toBeUndefined();
   // @ts-ignore
-  await expect(userRepository.delete(createdUser._id)).resolves.toBeUndefined();
+  await expect(profileRepository.delete(createdUser._id)).resolves.toBeUndefined();
   // @ts-ignore
-  await expect(userRepository.delete(createdUser._id)).rejects.toBeUndefined();
+  await expect(profileRepository.delete(createdUser._id)).rejects.toBeUndefined();
 });
-
-//
-// test('Create User positive test', async () => {
-//   const createdUser = await userRepository.create(validUser);
-//   expect(createdUser._id).not.toBeNaN();
-// });
-//
-// test('Create User with id negative test', async () => {
-//   await expect(userRepository.create(userWithId)).rejects.toBeUndefined();
-// });
-//
-//
-// test('Create User same email negative test', async () => {
-//   await userRepository.create(validUser);
-//   await expect(userRepository.create(validUser)).rejects.toBeUndefined();
-// });
-//
-// test('Read User positive test', async () => {
-//   let createdUser = await userRepository.create(validUser);
-//
-//   // @ts-ignore
-//   let readUser = await userRepository.read(createdUser._id);
-//   expect(readUser).toEqual(createdUser);
-// });
-//
-// test('Read User negative test', async () => {
-//   await expect(userRepository.read(new ObjectId().toString())).rejects.toBeUndefined();
-// });
-//
-// test('Read user by email positive test', async () => {
-//   let createdUser = await userRepository.create(validUser);
-//   await expect(userRepository.readByEmail(createdUser.email)).resolves.toEqual(createdUser);
-// });
-//
-// test('Read user by email negative test', async () => {
-//   await expect(userRepository.readByEmail("blabla")).rejects.toBeUndefined();
-// });
-//
-// test('Update user positive test', async () => {
-//   let createdUser = await userRepository.create(validUser);
-//   createdUser.email = faker.internet.email();
-//   await expect(userRepository.update(createdUser)).resolves.toBeUndefined();
-//   // @ts-ignore
-//   await expect(userRepository.read(createdUser._id)).resolves.toEqual(createdUser);
-// });
-//
-// test('Delete user positive test', async () => {
-//   let createdUser = await userRepository.create(validUser);
-//   // @ts-ignore
-//   await expect(userRepository.delete(createdUser._id)).resolves.toBeUndefined();
-//   // @ts-ignore
-//   await expect(userRepository.read(createdUser._id)).rejects.toBeUndefined();
-// });
-//
-// test('Delete user negative test', async () => {
-//   let createdUser = await userRepository.create(validUser);
-//   await expect(userRepository.delete(new ObjectId().toString())).rejects.toBeUndefined();
-//   // @ts-ignore
-//   await expect(userRepository.delete(createdUser._id)).resolves.toBeUndefined();
-//   // @ts-ignore
-//   await expect(userRepository.delete(createdUser._id)).rejects.toBeUndefined();
-// });
