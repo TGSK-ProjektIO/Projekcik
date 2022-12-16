43e6e55886071d9487b6770ea5398209feb26abc
levitatingmoon <94081701+levitatingmoon@users.noreply.github.com>
2022-12-16 10:05:53 +0100

    small change

--- express-backend-api/services/category.service.ts
+++ express-backend-api/services/category.service.ts
@@ -4,6 +4,7 @@ import {TYPES} from "../config/types.config";
 import {Category} from "../model/category";
 import moment from "moment";
 
+@injectable()
 export class CategoryService {
     constructor(@inject(TYPES.CategoryRepository) private categoryRepository: CategoryRepository) {
 
@@ -11,28 +12,28 @@ export class CategoryService {
 
     public addCategory(category: Category): Promise<Category> {
         return this.categoryRepository.create(category);
-      }
+        }
 
     public updateCategory(category: Category): Promise<void>  {
         return this.categoryRepository.update(category);
-      }
+        }
 
     public getCategory(id: string): Promise<Category> {
         return this.categoryRepository.read(id);
-      }
-  
+        }
+
     public deleteCategory(id: string): Promise<void> {
         return this.categoryRepository.delete(id);
-      }
+        }
 
     //for sure is wrong
     public getAllCategories(): Promise<Array<Category>> {
         return new Promise<Array<Category>>(async (resolve, reject) => {
-          try {
+            try {
             const categories = await this.categoryRepository.getAllCategories();
-          } catch (error) {
+            } catch (error) {
             reject();
-          }
+            }
         });
-      }
+        }
 }
