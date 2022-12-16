d3c47d028879a7b23d6db03cc0b6fcfd17888c16
levitatingmoon <94081701+levitatingmoon@users.noreply.github.com>
2022-12-16 18:14:50 +0100

    frontend continuation pt3

--- src/app/produkt/produkt-modyfikacja/produkt-modyfikacja.component.ts
+++ src/app/produkt/produkt-modyfikacja/produkt-modyfikacja.component.ts
@@ -9,10 +9,45 @@ import {Router} from "@angular/router";
 
 export class ProduktModyfikacjaComponent implements OnInit {
 
-  constructor() {
+  name = '';
+  description = '';
+  tag = '';
+  categoryName = '';
+  image = '';
+
+  constructor(private router: Router) {
     }
 
     ngOnInit(): void {
     }
 
+    redirectToMainPage() {
+      this.router.navigateByUrl('/');
+    }
+  
+    onSavePressed() {
+      fetch('http://localhost:3000//api/v1/produkt/product/:id', {
+        method: 'PUT',
+        headers: {
+          'Accept': '*/*',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "name": this.name,
+          "description": this.description,
+          "categoryName": this.categoryName,
+          "tag": this.tag,
+          "image": this.image
+        })
+      }).then(async response => {
+        if (response.status === 201) {
+          await this.router.navigateByUrl('/');
+        }
+        if (response.status === 404) {
+          console.log("hi");
+        }
+      }).catch(err => {
+        console.error(err);
+      });
+    }
 }
--- src/app/produkt/produkt-widok/produkt-widok.component.ts
+++ src/app/produkt/produkt-widok/produkt-widok.component.ts
@@ -8,9 +8,45 @@ import {Router} from "@angular/router";
 })
 export class ProduktWidokComponent implements OnInit {
 
-  constructor() { }
+  name = '';
+  description = '';
+  tag = '';
+  categoryName = '';
+  image = '';
 
-  ngOnInit(): void {
-  }
+  constructor(private router: Router) {
+    }
 
+    ngOnInit(): void {
+    }
+
+    redirectToMainPage() {
+      this.router.navigateByUrl('/');
+    }
+  
+    onSavePressed() {
+      fetch('http://localhost:3000/api/v1/produkt/product', {
+        method: 'POST',
+        headers: {
+          'Accept': '*/*',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "name": this.name,
+          "description": this.description,
+          "categoryName": this.categoryName,
+          "tag": this.tag,
+          "image": this.image
+        })
+      }).then(async response => {
+        if (response.status === 201) {
+          await this.router.navigateByUrl('/');
+        }
+        if (response.status === 404) {
+          console.log("hi");
+        }
+      }).catch(err => {
+        console.error(err);
+      });
+    }
 }
