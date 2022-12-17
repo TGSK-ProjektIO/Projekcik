1f44c44e5a1e4766dc388dc8d3191869b2a9e840
236662 <93220072+236662@users.noreply.github.com>
2022-12-17 12:19:44 +0100

    Update user.repository.ts

--- express-backend-api/repository/user.repository.ts
+++ express-backend-api/repository/user.repository.ts
@@ -152,7 +152,7 @@ export class UserRepository {
         } catch (exception) {
           reject();
         } finally {
-          await client.close();
+          client.close();
         }
     });
   }
